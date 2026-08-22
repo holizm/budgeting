9a010e78cc7ee6219d452df943101a664d195c61
Saeed Nemati <sn.1361@gmail.com>
2026-08-22 14:56:24 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='accountBalanceWallet'
+    name='budgeting'
+    title='budgetingBudgets'
+    {...props}
+/>
